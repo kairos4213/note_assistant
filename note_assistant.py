d356ade1be3e327f38e15d88dbd73c7167412a0f
kairos4213 <jamesdkairos@gmail.com>
2024-11-03 16:54:38 -0600

    Add test for custom file name

--- note_assistant.py
+++ note_assistant.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 
-def note_assistant(note, file_name="na.md"):
+def note_assistant(note, file_name="test"):
     path_home = Path.home()
     filename = file_name
-    file_path = Path(f"{path_home}/note_assistant/{filename}")
+    file_path = Path(f"{path_home}/note_assistant/{filename}.md")
 
     file_path.write_text(note)
--- test_note_assistant.py
+++ test_note_assistant.py
@@ -2,10 +2,16 @@ from pathlib import Path
 
 import note_assistant
 
+home_path = Path.home()
 
 def test_default_filename_and_path():
-    home_path = Path.home()
     test_file_path = Path(f"{home_path}/note_assistant/na.md")
     note_assistant.note_assistant("My random thought")
+
+    assert test_file_path.is_file()
+
+def test_custom_filename_and_path():
+    test_file_path = Path(f"{home_path}/note_assistant/test.md")
+    note_assistant.note_assistant("My random thought", file_name="test")
 
     assert test_file_path.is_file()
